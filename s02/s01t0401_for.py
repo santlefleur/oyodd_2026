54be4e93000d8fd4e718ffc19c94bc684e7a8bd3
santlefleur <142117874+santlefleur@users.noreply.github.com>
2026-09-22 16:25:57 +0000

    sum a totalsum

--- s02/s01t0401_for.py
+++ s02/s01t0401_for.py
@@ -1,21 +1,36 @@
 """
-Escrbir un programa que calcule 
+Escribir un programa que calcule 
 la suma de los "n" numeros naturales.
-por elemplo: si n = 100, el programa 
-calcularà la suma de 1 al 100
-42
+Por ejemplo si n = 100, el programa 
+calculará la suma de 1 al 100
 
 """
-# importamos bliblioteca time
+# Importamos bliblioteca time
 import time
 
 # Crear una marca de tiempo
 timestamp_01 = time.time()
-# progama que calcule las sumas 
+
+# Programa que calcule las sumas 
 # de los "n" numeros naturales 
-n= 100
+n = 100
 sum = 0
 
 #ciclo for
 for number in range(1,n+1):
-    print(str(number) + " ")
+    sum = sum + number 
+    # l: sum <- 0 + 1
+    # sum = 1
+    # 2: sum <- 1 + 2 
+    # sum = 3 
+    # 3: sum <- 3 +3 
+    # ...
+    # 100: SUM : <- sum_(-1) + 100
+
+print(f"la suma de 1 hasta {n} es: {sum}")
+
+#Tomando el tiempo final
+timestamp_02 = time.time()
+
+#Impresion del tiempo de ejecucion
+print(f"Tiempo de ejecución: {(timestamp_02 - timestamp_01) * 1e6} μs")
